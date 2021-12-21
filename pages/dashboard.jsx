19fa6582488cd73ba68d1274ae7c10e7e605ac32
Alex <ahern077@gmail.com>
2021-12-21 13:04:08 -0800

    changed color of stats

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -71,48 +71,52 @@ export default function Dashboard(){
                         <div className="flex flex-wrap space-x-1 justify-around">
                             {   
                                 cardContent.map( card => 
-                                    <div class="card bg-gray-800 compact w-1/4 ">
+                                    <div class="card bg-gray-800 compact w-1/4 relative mb-10">
                                         <div class="badge mx-2 badge-secondary absolute top-2 right-1 z-50">Best Bet</div>
-                                        <div class="card-body space-y-2">
-                                            <div className="bg-green-200 -mx-4 -mt-4 px-4 pt-4">
-                                                <h3 className="font-light text-green-500">Kickoff 12/13 at 7:20PM CST</h3> 
-                                                <h2 class="card-title text-base text-green-800 pt-2">{card.title}</h2>
+                                        <div class="card-body">
+                                            {/* banner text */}
+                                            <div className="bg-transparent -mx-4 -mt-4 px-4 pt-3 pb-3 z-10 h-1/4">
+                                                <h3 className="font-light text-gray-700/80">Kickoff 12/13 at 7:20PM CST</h3> 
+                                                <h2 class="card-title text-base text-black/80">{card.title}</h2>
                                             </div>
-                                            <div className="flex flex-col font-semibold text-gray-900 pt-3">
+                                            {/* banner bg */}
+                                            <div className="bg-green-200 absolute -mx-4 -top-3 w-full h-1/3 z-0"></div>
+                                            {/* stats chart */}
+                                            <div className="flex flex-col font-semibold text-gray-900 p-1 rounded-lg bg-gray-800 shadow-md z-10">
                                                 <div className="flex flex-row p-2 bg-gray-800 border-b-gray-700 border-b text-gray-300 min-w-full">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                     </svg>
-                                                    <div>Spread</div>
-                                                    <div className="bg-green-200 text-gray-700 rounded-md p-1 ml-auto">{card.spread}</div>
+                                                    <div className="text-gray-400 font-medium">Spread</div>
+                                                    <div className="border-green-500 border-2 text-green-700 rounded-md p-1 ml-auto">{card.spread}</div>
                                                 </div>
                                                 <div className="flex flex-row p-2 bg-gray-800 border-b-gray-700 border-b text-gray-300">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
                                                     </svg>
-                                                    <div>Winner</div>
-                                                    <div className="bg-yellow-200 text-gray-700 rounded-md p-1 ml-auto">{card.moneyLine}</div>
+                                                    <div className="text-gray-400 font-medium">Winner</div>
+                                                    <div className="border-amber-300 border-2 text-amber-500 rounded-md p-1 ml-auto">{card.moneyLine}</div>
                                                 </div>
                                                 <div className="flex flex-row p-2 bg-gray-800 border-b-gray-700 border-b text-gray-300">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                     </svg>
-                                                    <div>Over/Under</div>
-                                                    <div className="bg-red-200 text-gray-700 rounded-md p-1 ml-auto">{card.total}</div>
+                                                    <div className="text-gray-400 font-medium">Over/Under</div>
+                                                    <div className="border-rose-400 border-2 text-rose-500 rounded-md p-1 ml-auto">{card.total}</div>
                                                 </div>
                                                 <div className="flex flex-row p-2 bg-gray-800 border-b-gray-700 border-b text-gray-300 ">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                     </svg>
-                                                    <div>Best Props</div>
-                                                    <div className="bg-green-200 text-gray-700 rounded-md p-1 ml-auto">GB -7.5</div>
+                                                    <div className="text-gray-400 font-medium">Best Props</div>
+                                                    <div className="border-green-500 border-2 text-green-700 rounded-md p-1 ml-auto">GB -7.5</div>
                                                 </div>
                                                 <div className="flex flex-row p-2 bg-gray-800 border-b-gray-700 border-b text-gray-300 ">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                     </svg>
-                                                    <div>L10 ATS</div>
-                                                    <div className="bg-green-200 text-gray-700 rounded-md p-1 ml-auto">GB -7.5</div>
+                                                    <div className="text-gray-400 font-medium">L10 ATS</div>
+                                                    <div className="border-green-500 border-2 text-green-700 rounded-md p-1 ml-auto">GB -7.5</div>
                                                 </div>
                                             </div>
                                             <div id="weather" className="pt-5">
