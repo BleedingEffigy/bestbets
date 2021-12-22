3aff922e1d526897c260396ae44072c785ede49e
Alex <ahern077@gmail.com>
2021-12-22 12:22:16 -0800

    scaled dropdown cards

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -77,7 +77,7 @@ export default function Dashboard(){
                     </div> 
                     <div class="flex-1 px-2 mx-2 mb-0">
                         <div class="tabs">
-                            <a class="tab tab-bordered text-slate-100 border-b-white text-base">Models Picks</a> 
+                            <a class="tab tab-bordered text-slate-100 border-b-slate-200 text-base">Models Picks</a> 
                             <a class="tab tab-bordered text-slate-300">Results</a> 
                             <a class="tab tab-bordered text-slate-300">Where to Bet</a>
                             <a class="tab tab-bordered text-slate-300">Beat the Odds</a>
@@ -98,7 +98,7 @@ export default function Dashboard(){
             <main className="bg-slate-800 mx-auto w-11/12">
                     <section className="text-slate-400 bg-slate-800 body-font lg:relative py-8 px-12 z-50">
                         {/* dropdown */}
-                        <div class="dropdown">
+                        <div class="dropdown mb-4">
                             <div tabindex="0" class="m-1 btn">League
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
@@ -106,32 +106,32 @@ export default function Dashboard(){
                             </div> 
                             <div tabindex="0" class="p-0 shadow menu dropdown-content bg-slate-700 rounded w-96 divide-y divide-solid z-50">
                                 <div className="flex flex-row flex-wrap">
-                                    <div className="card w-2/3">
-                                            <div className="card-body border-8 border-transparent hover:border-red-400/80 hover:border-solid">
-                                                <h2 className="card-title text-red-300/80">🏀 NBA</h2>
+                                    <div className="card w-2/3 group">
+                                            <div className="card-body border-8 border-transparent ">
+                                                <h2 className="card-title text-red-300/80 group-hover:scale-125">🏀 NBA</h2>
                                             </div>
                                     </div>
                                 </div>
                                 <div className="bg-slate-800">
                                     <div className="flex flex-row flex-wrap">
-                                        <div className="card w-1/2">
-                                                <div className="card-body border-8 border-transparent hover:border-red-400/80 hover:border-solid">
-                                                    <h2 className="card-title text-red-300/80">🏀 NBA</h2>
+                                        <div className="card w-1/2 group">
+                                                <div className="card-body border-8 border-transparent ">
+                                                    <h2 className="card-title text-red-300/80 group-hover:scale-125">🏀 NBA</h2>
                                                 </div>
                                         </div>
-                                        <div className="card w-1/2">
-                                            <div className="card-body border-8 border-transparent hover:border-red-400/80 hover:border-solid">
-                                                <h2 className="card-title text-red-300/80">🏈 NFL</h2>
+                                        <div className="card w-1/2 group">
+                                            <div className="card-body border-8 border-transparent ">
+                                                <h2 className="card-title text-red-300/80 group-hover:scale-125">🏈 NFL</h2>
                                             </div>
                                         </div>
-                                        <div className="card w-1/2">
-                                            <div className="card-body border-8 border-transparent hover:border-red-400/80 hover:border-solid">
-                                                <h2 className="card-title text-red-300/80">⚾ MLB</h2>
+                                        <div className="card w-1/2 group">
+                                            <div className="card-body border-8 border-transparent ">
+                                                <h2 className="card-title text-red-300/80 group-hover:scale-125">⚾ MLB</h2>
                                             </div>
                                         </div>
-                                        <div className="card w-1/2">
-                                            <div className="card-body border-8 border-transparent hover:border-red-400/80 hover:border-solid">
-                                                <h2 className="card-title text-red-300/80">🏒 NHL</h2>
+                                        <div className="card w-1/2 group">
+                                            <div className="card-body border-8 border-transparent ">
+                                                <h2 className="card-title text-red-300/80 group-hover:scale-125">🏒 NHL</h2>
                                             </div>
                                         </div>
                                     </div>
@@ -281,7 +281,7 @@ export default function Dashboard(){
 
                     </div>
                     <div className="absolute mt-6">
-                    <button className="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">
+                    <button className="inline-block px-4 py-3 text-sm font-semibold text-center text-slate-200 uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">
                         <svg className="h-6 w-6" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Discord</title><path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"/></svg>
                         Sign in
                     </button>
